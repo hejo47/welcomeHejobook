5f2aed1e9ee671a992ef8470b4e313a0825d279d
hejo47 <hejo_47@naver.com>
2022-11-30 16:49:07 +0900

    221130 ContentsWrapper 추가

--- src/components/UI/ContentsContainer.js
+++ src/components/UI/ContentsContainer.js
@@ -2,12 +2,14 @@ import React from "react";
 import classes from "./ContentsContainer.module.css";
 import ContentsDesc from "./contentsUI/ContentsDesc";
 import ContentsTitle from "./contentsUI/ContentsTitle";
+import ContentsWrapper from "./contentsUI/ContentsWrapper";
 
 const ContentsContainer = () => {
   return (
     <div className={classes.content_container}>
       <ContentsTitle />
-      <ContentsDesc></ContentsDesc>
+      <ContentsDesc />
+      <ContentsWrapper></ContentsWrapper>
     </div>
   );
 };
