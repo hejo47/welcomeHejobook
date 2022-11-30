835ef1eaaa39f4983e93a7568f767b6e70a834b0
hejo47 <hejo_47@naver.com>
2022-11-30 16:48:09 +0900

    221130 contents wrapper 추가

---
+++ src/components/UI/contentsUI/ContentsWrapper.js
@@ -0,0 +1,7 @@
+import React from "react";
+import classes from "./ContentsWrapper.module.css";
+const ContentsWrapper = () => {
+  return <div className={classes.content}>ContentsWrapper</div>;
+};
+
+export default ContentsWrapper;
